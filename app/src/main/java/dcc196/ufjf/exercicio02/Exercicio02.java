3d02836f36d9065e41986a538f73a1a1a424de25
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-03-25 23:52:03 -0300

    Começando o exercício 2

---
+++ app/src/main/java/dcc196/ufjf/exercicio02/Exercicio02.java
@@ -0,0 +1,13 @@
+package dcc196.ufjf.exercicio02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercicio02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicio02);
+    }
+}
